ed74208bf4c3925f10c6e79d16da640b66426284
vicugna-pacos <50790874+vicugna-pacos@users.noreply.github.com>
2019-12-14 13:45:26 +0900

    メソッドの宣言だけ

---
+++ modules/shopping_list.js
@@ -0,0 +1,17 @@
+/**
+ * 買い物リストを開く
+ */
+module.exports.open = async function(page) {
+};
+
+/**
+ * 次の買うものを取得
+ */
+module.exports.next = async function(page) {
+};
+
+/**
+ * 商品をカートに入れたので完了にする
+ */
+module.exports.done = async function(page) {
+};
