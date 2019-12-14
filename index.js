d353bf0456763e758120a4aac29c17f6e5b9af8e
vicugna-pacos <50790874+vicugna-pacos@users.noreply.github.com>
2019-12-14 13:03:36 +0900

    ログイン追加

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-/*
-
-*/
+const puppeteer = require('puppeteer-core');
+const rakuten = require('./modules/rakuten.js');
 
 (async () => {
+    const LAUNCH_OPTION = {
+        headless : false
+       ,executablePath : 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
+   };
+
+	// キャッチされなかったPromiseのエラー詳細を出してくれる
+	process.on('unhandledRejection', console.dir);
+   
+    const browser = await puppeteer.launch(LAUNCH_OPTION);
+    try {
+        const page = await browser.newPage();   // 新しいタブを開く
+
+        // ログイン
+        await rakuten.login(page);
+
+
+        let param = {};
+        param["word"] = "ティッシュ";
+        param["sid"] = "261122";
+        param["units"] = 2;
+
+        await rakuten.order(page, param);
+
+    } finally {
+        browser.close();
+    }
 })();
--- modules/rakuten.js
+++ modules/rakuten.js
@@ -1,16 +1,19 @@
 const puppeteer = require('puppeteer-core');
+const config = require('config');
 
+// ログインURL
+const LOGIN_URL = "https://grp01.id.rakuten.co.jp/rms/nid/vc?__event=login&service_id=top";
 // 検索結果のURL　s=2は安い順
 const SEARCH_URL = "https://search.rakuten.co.jp/search/mall/[word]/?s=2&sid=[sid]";
 
 /**
-商品検索→カートに追加
-
-@param page puppeteerのPageオブジェクト
-@param param カートに入れたい商品の情報
-  word : 検索ワード
-  sid : ショップ番号
-  units : 数量
+ * 商品検索→カートに追加
+ * 
+ * @param page puppeteerのPageオブジェクト
+ * @param param カートに入れたい商品の情報
+ *   word : 検索ワード
+ *   sid : ショップ番号
+ *   units : 数量
 */
 module.exports.addCart = async function(page, param) {
 
@@ -51,4 +54,17 @@ module.exports.addCart = async function(page, param) {
     return true;
 };
 
+/**
+ * ログイン
+ */
+module.exports.login = async function(page) {
+    await page.goto(LOGIN_URL, {waitUntil:"domcontentloaded"});
 
+    // IDとパスワードを入力する
+    await page.type("#loginInner_u", config.rakuten.user_id, {delay:200});
+    await page.type("#loginInner_p", config.rakuten.user_id, {delay:200});
+
+    // ログインボタンを押す
+    await page.click("input[name=submit].loginButton");
+    await page.waitForNavigation({waitUntil:"domcontentloaded"});
+};
