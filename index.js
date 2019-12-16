b3747e39b8b53abe4fe3e42a2d4df1862e030cce
vicugna-pacos <50790874+vicugna-pacos@users.noreply.github.com>
2019-12-16 09:58:39 +0900

    全体の流れを作った

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const puppeteer = require("puppeteer-core");
 const config = require("config");
 const rakuten = require("./modules/rakuten.js");
 const goods = require("./modules/goods.js");
+const shopping_list = require("./modules/shopping_list.js");
 
 (async () => {
 
@@ -22,18 +23,34 @@ const goods = require("./modules/goods.js");
     try {
         const page = await browser.newPage();   // 新しいタブを開く
 
+        // 楽天市場にログイン
+        await rakuten.login(page);
+
         // 商品リストを開く
         goods.open();
 
-        // 楽天市場にログイン
-        await rakuten.login(page);
-        
-        let param = {};
-        param["word"] = "ティッシュ";
-        param["sid"] = "261122";
-        param["units"] = 2;
+        // 買い物リストを開く
+        await shopping_list.open(page);
 
-        await rakuten.order(page, param);
+        let shopping_item = await shopping_list.next(page);
+
+        while(shopping_item != null) {
+            // 商品リストを探す
+            let param = goods.search(shopping_item.key);
+            //param["word"] = "ティッシュ";
+            //param["sid"] = "261122";
+            //param["units"] = 2;
+
+            let succeed = await rakuten.addCart(page, param);
+
+            if (succeed) {
+                // 完了にする
+                await shopping_list.done(page);
+            }
+
+            // 次
+            shopping_item = await shopping_list.next(page);
+        }
 
     } finally {
         browser.close();
