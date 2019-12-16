fd4a03b7aa35911cecc41a27f028a5706189bc7e
vicugna-pacos <50790874+vicugna-pacos@users.noreply.github.com>
2019-12-16 09:10:46 +0900

    chromeのパスをconfigで指定可能にした

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const puppeteer = require('puppeteer-core');
-const rakuten = require('./modules/rakuten.js');
-const goods = require('./modules/goods.js');
+const puppeteer = require("puppeteer-core");
+const config = require("config");
+const rakuten = require("./modules/rakuten.js");
+const goods = require("./modules/goods.js");
 
 (async () => {
-    const LAUNCH_OPTION = {
-        headless : false
-       ,executablePath : 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
-   };
 
 	// キャッチされなかったPromiseのエラー詳細を出してくれる
-	process.on('unhandledRejection', console.dir);
+	process.on("unhandledRejection", console.dir);
    
+    let LAUNCH_OPTION = {
+        headless : false
+       ,executablePath : "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
+    };
+
+    if (config.chrome.executablePath) {
+        LAUNCH_OPTION.executablePath = config.chrome.executablePath;
+    }
+
     const browser = await puppeteer.launch(LAUNCH_OPTION);
+    
     try {
         const page = await browser.newPage();   // 新しいタブを開く
 
