d56fb3e71e4ef3973a9a5ec4421d79d3d0519047
vicugna-pacos <50790874+vicugna-pacos@users.noreply.github.com>
2020-01-03 16:13:45 +0900

    無駄なパースを回避してみた

--- modules/rakuten.js
+++ modules/rakuten.js
@@ -44,20 +44,7 @@ module.exports.getBookmarks = async function(page) {
             let memoArea = await container.$("div[class*=memoLeft]");
             if (memoArea != null) {
                 let memo = await memoArea.evaluate((node) => node.innerText);
-
-                try {
-                    let obj = JSON.parse(memo);
-                    if (obj["key"]) {
-                        bookmark.key = obj.key;
-                    }
-                    if (obj["units"]) {
-                        bookmark.units = obj.units;
-                    }
-                } catch (e) {
-                }
-                if (bookmark.key == null) {
-                    bookmark.key = memo;
-                }
+                bookmark = parseMemo(memo, bookmark);
             }
 
             // 購入ページへのリンク取得
@@ -88,20 +75,55 @@ module.exports.getBookmarks = async function(page) {
 };
 
 /**
+ * お気に入りのメモ欄をパースする
+ * 
+ * @param {string}} memo 
+ * @param {{key:string, units:number, url:string}} bookmark 
+ */
+function parseMemo(memo, bookmark) {
+    if (memo.length == 0) {
+        return bookmark;
+    }
+    if (memo.substring(0, 1) != "{") {
+        bookmark.key = memo;
+        return bookmark;
+    }
+
+    try {
+        let obj = JSON.parse(memo);
+        if (obj["key"]) {
+            bookmark.key = obj.key;
+        }
+        if (obj["units"]) {
+            bookmark.units = obj.units;
+        }
+    } catch (e) {
+    }
+    if (bookmark.key == null) {
+        bookmark.key = memo;
+    }
+
+    return bookmark;
+}
+
+/**
  * カートに追加
  * 
  * @param page puppeteerのPageオブジェクト
  * @param {{key:string, units:number, url:string}} param カートに入れたい商品の情報
+ * @returns {boolean} カート追加の成功or失敗
  */
 module.exports.addCart = async function(page, param) {
 
     try {
         // 商品ページへ行く
         await page.goto(param.url, {waitUntil:"domcontentloaded"});
-        await page.waitForSelector("select[name=units]", {"visible":true});
-        await page.waitForSelector("button.cart-button.add-cart", {"visible":true});
-        await page.waitFor(1000);
 
+        await Promise.all([
+            page.waitForSelector("select[name=units]", {"visible":true})
+            , page.waitForSelector("button.cart-button.add-cart", {"visible":true})
+        ]);
+        
         // 数量の選択
         if (param.units > 1) {
             await page.select("select[name=units]", String(param.units));
@@ -110,7 +132,7 @@ module.exports.addCart = async function(page, param) {
         // 注文を押す
         await page.click("button.cart-button.add-cart");
         // カートに追加しました　が出るまで待つ
-        await page.waitForSelector("div.add-cart-success", {visible:true});
+        await page.waitForSelector("div.add-cart-success", {"visible":true});
         await page.waitFor(1000);
 
     } catch(e) {
